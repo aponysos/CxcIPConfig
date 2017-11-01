45faee8c861d87aeddf58dd0eb90f9900eca53dc
aponysos <aponysos@163.com>
2017-11-01 17:14:43 +0800

    remove platform dir

---
+++ sync_3rdparty.bat
@@ -0,0 +1,6 @@
+xcopy .\lib\Debug\*.dll .\output\Debug\ /D
+xcopy .\lib\Debug\*.pdb .\output\Debug\ /D
+xcopy .\lib\Release\*.dll .\output\Release\ /D
+xcopy .\lib\Release\*.pdb .\output\Release\ /D
+
+pause
--- sync_log4cpp.bat
+++ sync_log4cpp.bat
@@ -1,9 +1,9 @@
-xcopy ..\log4cpp-msvc14\output\Debug\log4cpp.dll .\output\Win32\Debug /D
-xcopy ..\log4cpp-msvc14\output\Debug\log4cpp.pdb .\output\Win32\Debug /D
+xcopy ..\log4cpp-msvc14\output\Debug\log4cpp.dll .\lib\Debug /D
+xcopy ..\log4cpp-msvc14\output\Debug\log4cpp.pdb .\lib\Debug /D
 xcopy ..\log4cpp-msvc14\output\Debug\log4cpp.lib .\lib\Debug /D
 
-xcopy ..\log4cpp-msvc14\output\Release\log4cpp.dll .\output\Win32\Release /D
-xcopy ..\log4cpp-msvc14\output\Release\log4cpp.pdb .\output\Win32\Release /D
+xcopy ..\log4cpp-msvc14\output\Release\log4cpp.dll .\lib\Release /D
+xcopy ..\log4cpp-msvc14\output\Release\log4cpp.pdb .\lib\Release /D
 xcopy ..\log4cpp-msvc14\output\Release\log4cpp.lib .\lib\Release /D
 
 xcopy ..\log4cpp-msvc14\include .\include /D /S /E
