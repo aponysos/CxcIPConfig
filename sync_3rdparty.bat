4f2db9b355a2d14b8b72a4cee87800de991d8e4f
aponysos <aponysos@163.com>
2017-11-02 11:00:28 +0800

    add x64 config

--- sync_3rdparty.bat
+++ sync_3rdparty.bat
@@ -1,6 +1,11 @@
-xcopy .\lib\Debug\*.dll .\output\Debug\ /D /Y
-xcopy .\lib\Debug\*.pdb .\output\Debug\ /D /Y
-xcopy .\lib\Release\*.dll .\output\Release\ /D /Y
-xcopy .\lib\Release\*.pdb .\output\Release\ /D /Y
+xcopy .\lib\Debug\x64\*.dll .\output\Debug\x64\ /D /Y
+xcopy .\lib\Debug\x64\*.pdb .\output\Debug\x64\ /D /Y
+xcopy .\lib\Release\x64\*.dll .\output\Release\x64\ /D /Y
+xcopy .\lib\Release\x64\*.pdb .\output\Release\x64\ /D /Y
+
+xcopy .\lib\Debug\Win32\*.dll .\output\Debug\Win32\ /D /Y
+xcopy .\lib\Debug\Win32\*.pdb .\output\Debug\Win32\ /D /Y
+xcopy .\lib\Release\Win32\*.dll .\output\Release\Win32\ /D /Y
+xcopy .\lib\Release\Win32\*.pdb .\output\Release\Win32\ /D /Y
 
 pause
